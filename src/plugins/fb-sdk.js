07221dedbcf302da75c90fc3ccfef32550c850aa
Dillion Verma <dillionverma@hotmail.com>
2020-04-10 00:05:47 -0400

    facebook production app id

--- src/plugins/fb-sdk.js
+++ src/plugins/fb-sdk.js
@@ -5,7 +5,7 @@ import Vue from "vue";
 var FBPlugin = {};
 
 const options = {
-  appId: "1852298738119636",
+  appId: process.env.NODE_ENV == "production" ? "1702650673364269" : "1852298738119636",
   autoLogAppEvents: true,
   xfbml: true,
   version: "v3.2"
